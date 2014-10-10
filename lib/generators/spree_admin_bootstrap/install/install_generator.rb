46f5e9c90e149a02f5dd1f69f37e50d963cac4d7
Paul Richter <paul@Pauls-Mac-mini.local>
2014-10-10 03:11:27 -0400

    Updating installer to append to the correct file. Also temporarily removing old templates so they don't interfere, since they are very outdated at this point.

--- lib/generators/spree_admin_bootstrap/install/install_generator.rb
+++ lib/generators/spree_admin_bootstrap/install/install_generator.rb
@@ -3,21 +3,22 @@ module SpreeAdminBootstrap
     class InstallGenerator < Rails::Generators::Base
 
       def add_javascripts
-        append_file 'app/assets/javascripts/admin/all.js', "//= require admin/spree_admin_bootstrap\n"
+        append_file 'vendor/assets/javascripts/spree/backend/all.js', "//= require admin/spree_admin_bootstrap\n"
       end
 
       def add_stylesheets
-        remove_file 'app/assets/stylesheets/admin/all.css'
-        create_file 'app/assets/stylesheets/admin/all.css' do
-          "/*
-          * This is a manifest file that'll automatically include all the stylesheets available in this directory
-          * and any sub-directories. You're free to add application-wide styles to this file and they'll appear at
-          * the top of the compiled file, but it's generally better to create a new file per style scope.
-          *
-          *= require admin/spree_admin_bootstrap
-          */
-          "
-        end
+        # remove_file 'vendor/assets/stylesheets/spree/backend/all.css'
+        append_file 'vendor/assets/stylesheets/spree/backend/all.css', "*= require admin/spree_admin_bootstrap"
+        # create_file 'app/assets/stylesheets/admin/all.css' do
+        #   "/*
+        #   * This is a manifest file that'll automatically include all the stylesheets available in this directory
+        #   * and any sub-directories. You're free to add application-wide styles to this file and they'll appear at
+        #   * the top of the compiled file, but it's generally better to create a new file per style scope.
+        #   *
+        #   *= require admin/spree_admin_bootstrap
+        #   */
+        #   "
+        # end
       end
     end
   end
